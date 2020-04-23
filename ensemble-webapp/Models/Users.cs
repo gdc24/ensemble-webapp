0e5860174563dd94c295ec40a4bdefb0a7d880d7
gdc24 <gdc24@case.edu>
2020-04-23 18:59:25 -0400

    fix some member stuff

---
+++ ensemble-webapp/Models/Users.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ensemble_webapp.Models
+{
+    public class Users
+    {
+
+        public int IntUserID { get; set; }
+
+        public byte[] BytSalt { get; set; }
+
+        public byte[] BytKey { get; set; }
+
+        public string StrUsername { get; set; }
+
+        public string StrName { get; set; }
+
+        public string StrEmail { get; set; }
+
+        public int IntPhone { get; set; }
+    }
+}
