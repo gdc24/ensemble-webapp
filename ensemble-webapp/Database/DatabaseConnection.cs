e027ec0e8f001660703091ae3d511660b6333341
raa116 <raa116@case.edu>
2020-04-23 12:50:18 -0400

    moved sha256 hash function to new file to add onto it

--- ensemble-webapp/Database/DatabaseConnection.cs
+++ ensemble-webapp/Database/DatabaseConnection.cs
@@ -4,10 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-// below for ComputerSHA256Hash function
-using System.Text;
-using System.Security.Cryptography;
-
 namespace ensemble_webapp.Database
 {
     public class DatabaseConnection
@@ -26,30 +22,6 @@ namespace ensemble_webapp.Database
                 "Database=" + DATABASE + ";");
 
             return conn;
-        }
-        
-        // Compute hash of a string using SHA 256
-        public static ComputeSHA256Hash(string toHash, byte[] salt)
-        {
-            using (SHA256 hash = SHA256.Create())
-            {
-                // Computer hash in byte form
-                byte[] b = hash.ComputeHash(Encoding.UTF8.GetBytes(toHash));
-
-                // Concatenate toHash and salt byte arrays
-                byte[] full = new int[b.Length + salt.Length];
-                Array.Copy(b, full, b.Length);
-                Array.Copy(salt, full, b.Length, salt.Length);
-
-                // Convert byte array hash back to string
-                StringBuilder builder2 = new StringBuilder();
-                for (int i = 0; i < full.Length; i++)
-                {
-                    builder2.Append(full[i].ToString("x2"));
-                }
-
-                return builder2.ToString();
-            }
         }
     }
 }
---
+++ ensemble-webapp/Database/Login.cs
@@ -0,0 +1,35 @@
+// below for ComputerSHA256Hash function
+using System.Text;
+using System.Security.Cryptography;
+
+namespace ensemble_webapp.Database
+{
+
+    public class Login
+    {
+
+        // Compute hash of a string using SHA 256
+        public static ComputeSHA256Hash(string toHash, byte[] salt)
+        {
+            using (SHA256 hash = SHA256.Create())
+            {
+                // Computer hash in byte form
+                byte[] b = hash.ComputeHash(Encoding.UTF8.GetBytes(toHash));
+
+                // Concatenate toHash and salt byte arrays
+                byte[] full = new int[b.Length + salt.Length];
+                Array.Copy(b, full, b.Length);
+                Array.Copy(salt, full, b.Length, salt.Length);
+
+                // Convert byte array hash back to string
+                StringBuilder builder2 = new StringBuilder();
+                for (int i = 0; i < full.Length; i++)
+                {
+                    builder2.Append(full[i].ToString("x2"));
+                }
+
+                return builder2.ToString();
+            }
+        }
+    }
+}
