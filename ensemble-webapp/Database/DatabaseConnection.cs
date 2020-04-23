71b053ead9db2f9911e31ce6b3d9ffbbbdd72289
raa116 <raa116@case.edu>
2020-04-23 12:22:16 -0400

    added in computing hash of string and salt function to DatabaseConnection.cs

--- ensemble-webapp/Database/DatabaseConnection.cs
+++ ensemble-webapp/Database/DatabaseConnection.cs
@@ -3,6 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Text;
+using System.Security.Cryptography;
+using System.Security.Policy;
 
 namespace ensemble_webapp.Database
 {
@@ -22,6 +25,30 @@ namespace ensemble_webapp.Database
                 "Database=" + DATABASE + ";");
 
             return conn;
+        }
+        
+        // Compute hash of a string using SHA 256
+        public static ComputeSHA256Hash(string toHash, byte[] salt)
+        {
+            using (SHA256 hash = SHA256.Create())
+            {
+                // Computer hash in byte form
+                byte[] b = hash.ComputeHash(Encoding.UTF8.GetBytes(toHash));
+
+                // Concatenate toHash and salt byte arrays
+                byte[] full = new int[b.Length + salt.Length];
+                Array.Copy(b, full, b.Length);
+                Array.Copy(salt, full, b.Length, salt.Length);
+
+                // Convert byte array hash back to string
+                StringBuilder builder2 = new StringBuilder();
+                for (int i = 0; i < full.Length; i++)
+                {
+                    builder2.Append(full[i].ToString("x2"));
+                }
+
+                return builder2.ToString();
+            }
         }
     }
 }
