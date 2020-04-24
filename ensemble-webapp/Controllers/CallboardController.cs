5b81f30cf7c681b09a7144dfdeadfcdcf6ff2f4b
raa116 <raa116@case.edu>
2020-04-24 15:52:30 -0400

    did callboard stuff

--- ensemble-webapp/Controllers/CallboardController.cs
+++ ensemble-webapp/Controllers/CallboardController.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using ensemble_webapp.Database;
+using ensemble_webapp.Models;
+using ensemble_webapp.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,6 +15,17 @@ namespace ensemble_webapp.Controllers
         // GET: Callboard
         public ActionResult Index()
         {
+            CallboardHomeVM model = new CallboardHomeVM();
+            model.CurrentUser = Globals.LOGGED_IN_USER;
+
+            GetDAL get = new GetDAL();
+            get.OpenConnection();
+
+            foreach (Event e in model.CurrentUser.LstEvents)
+            {
+                model.LstAllCallboards.Concat(get.GetCallboardsByEvent(e));
+            }
+
             return View();
         }
     }
