c3a30f177ff8ef652aafd93125e8715075f42c3b
nac71 <43797817+nac71@users.noreply.github.com>
2020-04-24 19:09:57 -0400

    Update ReportsController.cs

--- ensemble-webapp/Controllers/ReportsController.cs
+++ ensemble-webapp/Controllers/ReportsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using IronPdf;
 
 namespace ensemble_webapp.Controllers
 {
@@ -12,6 +13,15 @@ namespace ensemble_webapp.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        
+        public ActionResult MakeReport(int rehearsalID){
+            
+        }
+        
+        public ActionResult GenerateReport(){
+            var Renderer = new IronPdf.HtmlToPdf();
+            Renderer.RenderHtmlAsPdf("ensemble-webapp/ensemble-webapp/Views/Reports/GenerateReport.cshtml").SaveAs("Report.pdf");
         }
     }
 }
