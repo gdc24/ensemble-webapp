97abe600d9ec805495931f14d9441ece289dfaa4
gdc24 <gdc24@case.edu>
2020-04-23 22:36:36 -0400

    log in fixes

--- ensemble-webapp/Controllers/HomeController.cs
+++ ensemble-webapp/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using ensemble_webapp.Database;
 using ensemble_webapp.Models;
+using ensemble_webapp.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,18 +30,45 @@ namespace ensemble_webapp.Controllers
             return View();
         }
 
+        public ActionResult Login()
+        {
+            LoginVM model = new LoginVM();
+
+            GetDAL get = new GetDAL();
+            get.OpenConnection();
+            model.LstAllEvents = get.GetAllEvents();
+
+            get.CloseConnection();
+
+            return View("Login", model);
+        }
+
+        [HttpPost]
         public ActionResult LoginUser(Users logInUser)
         {
-            if (Login.VerifyUser(logInUser.StrUsername, logInUser.StrPassword)) {
+            if (Database.Login.VerifyUser(logInUser.StrUsername, logInUser.StrPassword)) {
                 return RedirectToAction("Index");
             }
 
-            return View();
+            ProfileHomeVM model = new ProfileHomeVM();
+
+            GetDAL get = new GetDAL();
+            get.OpenConnection();
+
+            Globals.LOGGED_IN_USER = get.GetUserByName(logInUser.StrUsername);
+            model.LstAllEvents = get.GetAllEvents();
+
+            get.CloseConnection();
+
+            model.CurrentUser = Globals.LOGGED_IN_USER;
+            model.EditedUserProfile = model.CurrentUser;
+
+            return View("Profile/ProfileHome", model);
         }
 
         public ActionResult NewUser(Users newUser)
         {
-            if (Login.CreateUser(newUser.StrUsername, newUser.StrPassword, newUser.LstEvents)) {
+            if (Database.Login.CreateUser(newUser.StrUsername, newUser.StrPassword, newUser.LstEvents)) {
                 return RedirectToAction("Index");
             }
 
@@ -49,7 +77,7 @@ namespace ensemble_webapp.Controllers
 
         public ActionResult Logout()
         {
-            if (Login.Logout())
+            if (Database.Login.Logout())
             {
                 return RedirectToAction("Login");
             }
