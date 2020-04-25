90f57e8a1ef1666f82706525eaf8ca1c9764ae05
gdc24 <gdc24@case.edu>
2020-04-25 13:35:56 -0400

    added some buttons to the profile page

--- ensemble-webapp/App_Start/BundleConfig.cs
+++ ensemble-webapp/App_Start/BundleConfig.cs
@@ -33,7 +33,8 @@ namespace ensemble_webapp
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/global.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
