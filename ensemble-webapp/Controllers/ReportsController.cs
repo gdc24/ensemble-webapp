8177ac837306fb85ab450e1817ffaf05ea36fe1b
nac71 <43797817+nac71@users.noreply.github.com>
2020-04-27 00:48:11 -0400

    Update ReportsController.cs
    added to MakeReport method

--- ensemble-webapp/Controllers/ReportsController.cs
+++ ensemble-webapp/Controllers/ReportsController.cs
@@ -25,6 +25,44 @@ namespace ensemble_webapp.Controllers
         
         //add text to GenerateReport.cshtml
         public ActionResult MakeReport(int rehearsalID){
+            
+            ReportsHomeVM model = new ReportsHomeVM();
+            model.CurrentUser = Globals.LOGGED_IN_USER;
+            
+            GetDAL get = new GetDAL();
+            get.OpenConnection();
+
+            Rehearsal r = get.GetRehearsalbyID(rehearsalID);
+            List<RehearsalPart> pLst = GetRehearsalPartsByRehearsal(r);
+
+            model.EventName = pLst.item[0].@event.strEventName;
+
+            model.RehearsalDate = get.GetRehearsalByID(rehearsalID).dtmStartDateTime.Date.toString();
+
+            model.GroupName = pLst.item[0].@event.group;
+
+            model.Location = get.GetRehearsalByID(rehearsalID).strLocation;
+
+            model.StartTime = get.GetRehearsalByID(rehearsalID).dtmStartDateTime.TimeOfDay.toString();
+
+            model.EndTime = get.GetRehearsalByID(rehearsalID).dtmStartDateTime.TimeOfDay.toString();
+
+            for(int i = 0; pLst.item[i] != null; i++){
+
+                model.PlannedAttendance = model.PlannedAttendance.AddRage(get.GetAttendancePlannedByRehearsalPart(pLst.item[i]));
+            
+            }
+
+            for(int j = 0; pLst.item[j] != null; j++){
+
+                model.ActualAttendance = model.ActualAttendance.AddRage(get.GetAttendanceActualByRehearsalPart(pLst.item[j]));
+            
+            }
+            
+            model.Notes = get.GetRehearsalByID(rehearsalID).strNotes;
+
+            get.CloseConnection();
+            
             return RedirectToAction("Index");
 
             
