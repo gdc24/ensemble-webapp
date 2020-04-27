508119d9040bd4a4669fd0e5304c5d9d0303a414
nac71 <43797817+nac71@users.noreply.github.com>
2020-04-27 00:17:54 -0400

    Update ReportsHomeVM.cs
    added fields needed for report

--- ensemble-webapp/ViewModels/ReportsHomeVM.cs
+++ ensemble-webapp/ViewModels/ReportsHomeVM.cs
@@ -7,5 +7,20 @@ namespace ensemble_webapp.ViewModels
 {
     public class ReportsHomeVM
     {
+        public String EventName { get; set; }
+
+        public String RehearsalDate { get; set; }
+        
+        public String GroupName { get; set; }
+        
+        public String Location { get; set; }
+        
+        public String StartToEnd { get; set; }
+
+        public AttendancePlanned PlannedAttendance { get; set; }
+
+        public AttendanceActual ActualAttendance { get; set; }
+        
+        public String Notes { get; set; }
     }
 }
